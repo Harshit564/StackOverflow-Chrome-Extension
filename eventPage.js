bfed352da2beb73352762f94a0a1676618760ce4
Harshit Singh <harshitsingh15967@gmail.com>
2020-05-17 15:09:40 +0530

    Initial Commit

---
+++ eventPage.js
@@ -0,0 +1,25 @@
+var menuItem ={
+	"id": "SS",
+	"title": "Stackoverflow Search",
+	"contexts": ["selection"]
+};
+chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI (str){
+	return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+	if (clickData.menuItemId == "SS" && clickData.selectionText){
+		var stackUrl ="https://stackoverflow.com/" + fixedEncodeURI(clickData.selectionText);
+		var createData = {
+			"url": stackUrl,
+			"type": "popup",
+			"top": 5,
+			"left": 5,
+			"width": screen.availHeight/2,
+			"height": screen.availHeight/2
+		};
+		chrome.windows.create(createData, function(){});
+	}
+});
